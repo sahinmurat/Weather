eb0f29724d079c7aed1945bebc81ce490680ec5f
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-13 20:45:14 +0100

    cardstyle used

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import CardStyle from './Card.style.css'
 
 function Card() {
     return (
-        <div style={{ color:'blue', backgroundColor:'orange'}}  >
+        <div className='card-wrapper'  >
             <h1> Test    </h1>
         </div>
     )
