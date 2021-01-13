2ab8eb3aa766b19efca69b9db09998791f030e69
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-13 20:25:27 +0100

    card component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import {SignIn} from './pages/SignIn'
-import {SignUp} from './pages/SignUp'
+import Card from './components/Card/index'
 
 function App() {
   return (
     <div className="App">
-     {/* <SignIn /> */}
-     <SignUp />
-      
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className='card-wrapper'  >
+            <h1> Test    </h1>
+        </div>
+    )
+}
+
+export default Card
